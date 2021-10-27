79dec488b0380caef06703d61c0f85be066fd316
Jack Lynch <jrlynch00@gmail.com>
2021-10-27 14:43:00 -0400

    Fixed new message container to bottom and made app responsive.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { createTheme } from '@mui/material';
 
-import Chat from './Chat';
+import Chat from './chat/Chat';
+import { ThemeProvider } from '@mui/styles';
+
+const theme = createTheme({
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 600,
+			md: 900,
+			lg: 1200,
+			xl: 1536,
+		},
+	},
+});
 
 const App = () => {
-	return <Chat />;
+	return (
+		<ThemeProvider theme={theme}>
+			<Chat />
+		</ThemeProvider>
+	);
 };
 
 export default App;
